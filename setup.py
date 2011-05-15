b314407496a486b8c80032ab4dae504a394be0df
pydanny <pydanny@gmail.com>
2011-05-15 14:58:44 -0700

    more files

--- setup.py
+++ setup.py
@@ -1,33 +1,4 @@
-README = """==============================
-THAT - the Python anti-zen
-==============================
-
-Warning: This is a joke. Don't use these bits unless you want to be hated.
-
-Installation
-============
-
-At some point this will be packaged up and placed on PyPI. Then you'll be able to just do::
-
-    pip install that
-
-Or if you want to try and play with the awesome source code::
-
-    git clone git@github.com:pydanny/that.git
-
-Usage
-======
-
-From the Python shell::
-
-    import that
-
-Enjoy the wisdom!
-
-Contributing
-============
-
-Clearly, the code that generates `THAT` is too straightforward. If you can make it better, then do so. Just don't do anything like add tests or document the code."""
+README = open("README.rst")
 
 import os
 try:
@@ -37,7 +8,7 @@ except ImportError:
 
 setup(
     name = 'Python Wars Solo',
-    version = '1.0.3',
+    version = '1.0.0-alpha',
     description = "A retro-style Apple ][ Basic game cooked up in 45 minutes",
     license = 'GPL',
     long_description = README,
